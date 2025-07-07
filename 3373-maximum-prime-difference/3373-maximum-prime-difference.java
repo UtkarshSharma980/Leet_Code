325752d0ede7b2c5edffb873b496e2292cfa6b18
Utkarsh Sharma <122614349+UtkarshSharma980@users.noreply.github.com>
2025-07-07 10:31:47 +0530

    Time: 2 ms (48.65%), Space: 79.6 MB (30.94%) - LeetHub

--- 3373-maximum-prime-difference/3373-maximum-prime-difference.java
+++ 3373-maximum-prime-difference/3373-maximum-prime-difference.java
@@ -1,9 +1,10 @@
 class Solution {
     public int maximumPrimeDifference(int[] nums) {
         int p=-1,q=-1;
+        int n=nums.length;
         for(int i=0;i<nums.length;i++){
             if(p==-1 && isPrime(nums[i])) p=i;
-            if(q==-1 && isPrime(nums[nums.length-1-i])) q=nums.length-1-i;
+            if(q==-1 && isPrime(nums[n-1-i])) q=n-1-i;
         }
         return q-p;
     }
